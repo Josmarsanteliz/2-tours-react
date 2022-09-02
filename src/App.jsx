7963cc27605350e94fcb3efb1e25de2de04abd94
Josmarsanteliz <67925913+Josmarsanteliz@users.noreply.github.com>
2022-09-02 10:23:52 -0400

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
 import './index.css'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
+//josmarsanteliz
+
 const url = 'https://www.course-api.com/react-tours-project'
 
 
